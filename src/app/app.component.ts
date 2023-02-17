37e4233a51eef38aa688871f17fce9b0e995ad2c
Kobi Cohen <kobi.cohen@sap.com>
2023-02-17 03:12:33 +0200

    basic app ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 
+declare const gigya:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'b2b-demo';
+
+  user: any;
+  constructor(private ref: ChangeDetectorRef) {}
+  ngOnInit(): void {
+    gigya.accounts.addEventHandlers({
+      onLogin: (event:any) => {
+        console.log(event);
+        this.user = event.profile;
+        this.ref.detectChanges();
+      }
+    });
+    gigya.accounts.getAccountInfo({
+      callback: (response:any) => {
+        console.log(response);
+        if(response.errorCode === 0) {
+          this.user = response.profile;
+          this.ref.detectChanges();
+        }
+      }
+    });
+  }
+
+  registerOrg(): void {
+    gigya.accounts.showScreenSet({
+      screenSet: 'Default-OrganizationRegistration',
+      containerID: 'registerOrg'
+    });
+  }
+
+  login() {
+    gigya.accounts.showScreenSet({
+      screenSet: 'Default-RegistrationLogin'
+    })
+  }
+
+  showProfile() {
+    gigya.accounts.showScreenSet({
+      screenSet: 'Default-ProfileUpdate'
+    })
+  }
+
+  logout() {
+    gigya.accounts.logout();
+    this.user = undefined;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from "@angular/material/menu";
+
+
 
 @NgModule({
   declarations: [
@@ -10,9 +20,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
